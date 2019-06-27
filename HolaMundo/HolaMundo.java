681dedbd1f18ad72f80968740abfb499c9a54b11
angelblasco <angel.blasco.m@outlook.com>
2019-06-27 09:44:40 +0200

    se incluye eñl programa Hola Mundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,7 @@
+/* Programa de iniciacion */
+/* Al ejecutarse devuelve la sentencia Hola Mundo */
+public class HolaMundo {
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!");
+	}
+}
